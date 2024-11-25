b537061119dea48eedada7b878f074b3f7995e89
hashan789 <hashanmalind123@gmail.com>
2024-11-25 17:21:03 +0530

    added more projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,7 @@
 
 const projects = [
   { title: "Financial research assistant", description: "AI-driven healthcare app.", link: "#" },
-  { title: "Web Scraper", description: "Automated data scraping tool.", link: "#" },
+  { title: "Doc Query AI", description: "Automated data scraping tool.", link: "#" },
   { title: "Python AI chatbot", description: "Automated data scraping tool.", link: "#" },
   { title: "Email spam classifier", description: "Automated data scraping tool.", link: "#" },
 ];
@@ -12,14 +12,14 @@ const Projects = () => (
   <section id="projects" className="py-20">
     <div className="max-w-6xl mx-auto text-center">
       <h2 className="text-3xl font-bold mb-6">Projects</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+      <div className="flex justify-center items-center gap-8">
         {projects.map((project) => (
-           <div key={project.title} className="p-4 shadow-lg">
-             <h3 className="text-xl font-semibold">{project.title}</h3>
-             <p className="text-gray-600">{project.description}</p>
+           <div key={project.title} className="p-4 shadow-xl">
+             <h3 className="text-lg font-semibold">{project.title}</h3>
+             <p className="text-gray-600 text-sm">{project.description}</p>
              <a
                href={project.link}
-               className="text-blue-600 hover:underline mt-2 block"
+               className="text-blue-600 text-xs hover:underline mt-2 block"
              >
                View Details
              </a>
