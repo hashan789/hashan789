27bd744b8097bdb406a1966c66b536be6a9ee351
hashan789 <hashanmalind123@gmail.com>
2024-11-25 17:16:34 +0530

    added work experiences

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import WorkExperienceTree from "./components/Experience";
 
 const App = () => (
   <div className="font-poppins">
     <Navbar />
     <Hero />
     <About />
+    <WorkExperienceTree />
     <Projects />
     <Contact />
     <Footer />
