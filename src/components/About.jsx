a7ac89474f4b42bdb637e86683b7b8a6de66d4f5
hashan789 <hashanmalind123@gmail.com>
2024-11-25 17:16:54 +0530

    updated about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,13 @@
 const About = () => (
     <section id="about" className="py-20 bg-gray-100">
-      <div className="max-w-4xl mx-auto text-center">
-        <h2 className="text-3xl font-bold mb-4">About Me</h2>
-        <p className="text-gray-700">
-          I'm a highly skilled and versatile professional with a strong foundation in software development, artificial intelligence, and web technologies. He is deeply invested in leveraging cutting-edge technology to solve real-world problems, particularly in the fields of healthcare and data-driven systems.
+      <div className="flex justify-evenly items-center">
+        <div className="w-32 h-32 rounded-full bg-slate-400 border"></div>
+        <div className="align-left">
+        <h2 className="text-3xl font-bold mb-4 text-center">About Me</h2>
+        <p className="text-gray-700 max-w-2xl">
+          I'm a highly skilled and versatile professional with a strong foundation in software development, artificial intelligence, and web technologies. I am deeply invested in leveraging cutting-edge technology to solve real-world problems, particularly in the fields of healthcare and data-driven systems.
         </p>
+        </div>
       </div>
     </section>
   );
