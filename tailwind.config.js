672a6f7042d8ea26a15acfaac2f79e6247d0d7e2
hashan789 <hashanmalind123@gmail.com>
2024-12-02 10:50:22 +0530

    added css styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,25 @@ module.exports = {
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'], // Set as default sans-serif font
       },
+      keyframes: {
+        slideInLeft: {
+          "0%": { transform: "translateX(-100%)" },
+          "100%": { transform: "translateX(0)" },
+        },
+        slideInRight: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0)" },
+        },
+        fadeIn: {
+          "from" : { opacity: 0 },
+          "to" : { opacity: 1 }
+        }
+      },
+      animation: {
+        "slide-in-left": "slideInLeft 1s ease-out",
+        "slide-in-right": "slideInRight 1s ease-out",
+        "fade-in": "fadeIn 1s ease-in-out",
+      },
     },
   },
   plugins: [],
