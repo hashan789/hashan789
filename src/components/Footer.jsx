523452b871ed2e3fe9a2b0f9c9695548f4cf0d9c
hashan789 <hashanmalind123@gmail.com>
2025-01-12 01:19:37 +0530

    updated social media links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,16 @@
-import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaMedium } from "react-icons/fa";
 
 const Footer = () => (
   <footer className="py-6 bg-gray-800 text-white text-center">
     <div className="flex justify-center items-center space-x-4">
-      <a href="#3" className="text-gray-400 hover:text-white text-2xl">
+      <a href="https://github.com/hashan789" className="text-gray-400 hover:text-white text-2xl">
         <FaGithub />
       </a>
-      <a href="#1" className="text-gray-400 hover:text-white text-2xl">
+      <a href="https://www.linkedin.com/in/hashan-malinda-b89474213/" className="text-gray-400 hover:text-white text-2xl">
         <FaLinkedin />
       </a>
-      <a href="#2" className="text-gray-400 hover:text-white text-2xl">
-        <FaTwitter />
+      <a href="https://medium.com/@hashanmalind123" className="text-gray-400 hover:text-white text-2xl">
+        <FaMedium />
       </a>
     </div>
     <p className="text-gray-500 mt-4">&copy; 2024 Hashan. All Rights Reserved.</p>
