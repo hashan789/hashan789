68ec62b8710294b8251b0b0feff9e8dd188b6dbd
hashan789 <hashanmalind123@gmail.com>
2025-01-15 20:17:49 +0530

    modified about me description

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ const About = () => (
         <div className="align-left">
         <h2 className="text-3xl font-bold mb-4 text-center">About Me</h2>
         <p className="text-gray-700 max-w-2xl">
-        I am a passionate and results-driven software developer with expertise in Microsoft Azure, React.js, and Python, coupled with a strong foundation in Machine Learning. My experience spans designing and deploying cloud-based solutions, building dynamic and responsive web applications, and developing predictive models that drive data-driven decision-making.
+        I am a full-stack developer with expertise in Microsoft Azure, React.js, and Python, coupled with a strong foundation in Machine Learning. My experience spans designing and deploying cloud-based solutions, building dynamic and responsive web applications, and developing predictive models that drive data-driven decision-making.
         </p>
         </div>
       </div>
