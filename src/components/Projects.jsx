49b932a75c60eb1dabbebbdd709d9a409f2da14d
hashan789 <hashanmalind123@gmail.com>
2025-01-15 20:18:59 +0530

    modified slides animation

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,5 @@
 // import { Card } from "@shadcn/ui";
 import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import Slider from "react-slick";  
 import logo from '../images/custom_gpt_image1.png';
 import projects from '../documents/projects.json'
@@ -23,28 +20,29 @@ function Projects(){
   return(
   <section id="projects" className="py-20">
     <div className="text-3xl text-center font-bold mb-6">Projects</div>
-    <Box className="font-poppins max-w-5xl mx-auto mt-10 p-4">
+    <Box className="font-poppins mx-auto mt-4 p-4">
       <Slider {...settings}>
         {projects.map((project, index) => (
-          <Box key={index} className="p-4">
-            <Card className="shadow-lg">
-              <CardMedia
-                component="img"
-                height="200"
-                image={logo}
-                alt={project.title}
-              />
-              <CardContent>
-                <h5 className="font-poppins font-bold">
+          <div key={index} className="p-4">
+            <div className="shadow-lg flex items-center justify-center">
+              <div className='w-7/12 h-full'>
+                <img
+                  className="w-full h-full object-cover"
+                  src={logo}
+                  alt={project.title}
+                />
+              </div>
+              <div className="w-5/12 p-5 ml-5">
+                <h5 className="font-poppins font-bold mb-5">
                   {project.title}
                 </h5>
-                <p className="font-poppins text-gray-600">
+                <p className="font-poppins text-gray-600 mb-5">
                   {project.description}
                 </p>
-                <a href={project.link} className='text-cyan-700 text-sm mt-5'>View Source Code</a>
-              </CardContent>
-            </Card>
-          </Box>
+                <a href={project.link} className='text-cyan-700 text-sm mt-5 mb-5'>View Source Code</a>
+              </div>
+            </div>
+          </div>
         ))}
       </Slider>
     </Box>
