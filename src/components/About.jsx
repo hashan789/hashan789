690e12805e6624db15542969e840e02c842f07a0
hashan789 <hashanmalind123@gmail.com>
2025-01-24 22:25:36 +0530

    fixed text style

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,11 @@
 const About = () => (
     <section id="about" className="py-20 bg-gray-100">
-      <div className="sm:block lg:flex justify-evenly items-center text-center">
+      <div className="sm:block lg:flex justify-evenly items-center max-sm:text-center">
         <div className="w-32 h-32 rounded-full bg-slate-400 border max-sm:w-0 max-sm:h-0"></div>
         <div className="sm:block align-left">
         <h2 className="text-3xl font-bold mb-4 text-center">About Me</h2>
-        <div className="w-32 h-32 rounded-full bg-slate-400 border max-sm:relative max-sm:left-1/3 max-sm:mb-5"></div>
-        <p className="text-gray-700 max-w-2xl max-sm:mx-5 max-sm:text-sm lg:text-xl">
+        <div className="w-0 h-0 rounded-full bg-slate-400 border max-sm:w-32 max-sm:h-32 max-sm:relative max-sm:left-1/3 max-sm:mb-5"></div>
+        <p className="text-gray-700 max-w-2xl max-sm:mx-5 max-sm:text-sm">
         I am a full-stack developer with expertise in Microsoft Azure, React.js, and Python, coupled with a strong foundation in Machine Learning. My experience spans designing and deploying cloud-based solutions, building dynamic and responsive web applications, and developing predictive models that drive data-driven decision-making.
         </p>
         </div>
