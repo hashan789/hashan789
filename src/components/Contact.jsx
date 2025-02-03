4a1ac7d5df9e7e58c39714066b7db479d1e3497c
hashan789 <hashanmalind123@gmail.com>
2025-02-03 22:52:59 +0530

    created a description for 'contact me' form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,8 +4,10 @@ import logo from '../images/contact_image.png';
 const Contact = () => (
     <section id="contact" className="flex items-center justify-center py-20 bg-gray-200 text-white">
       <div className="container max-sm:block flex items-center justify-around rounded-lg bg-gray-500" style={{ backgroundImage : `url(${logo})` , backgroundPositionY : 'center' , }}>
-      <div className="w-6/12 max-sm:none bg-no-repeat bg-cover bg-center">
-        {/* <img src={logo} alt={'logo'} width={1000} height={1000} /> */}
+      <div className="w-6/12 max-sm:none bg-no-repeat bg-cover bg-center bg-opacity-50 space-x-5">
+        <p className="text-6xl font-bold mb-5 text-center">Gives Best</p>
+        <p className="text-sm mb-5">Have a question, project idea, or collaboration opportunity? Feel free to reach out! Whether you want to discuss Microsoft Azure solutions, AI-driven applications, React.js development, or machine learning projects, I’d love to hear from you. Fill out the form below, and I’ll get back to you as soon as possible.</p>
+        <p className="text-sm">Let{"’"}s build something amazing together! 🚀</p>
       </div>
       <div className="w-6/12 max-sm:w-full block p-20">
         <h2 className="text-3xl text-center font-bold mb-6">Contact Me</h2>
