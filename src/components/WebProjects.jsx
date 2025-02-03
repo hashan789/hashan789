92bd49308c5f264345ae88a3ca42cb3047bf6f71
hashan789 <hashanmalind123@gmail.com>
2025-02-03 22:23:51 +0530

    created template for web development

---
+++ src/components/WebProjects.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import Box from '@mui/material/Box';
+import Slider from "react-slick";  
+import logo from '../images/custom_gpt_image1.png';
+import projects from '../documents/webprojects.json'
+
+export default function WebProjects() {
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+        };
+
+  return (
+    <div>
+        <Box className="font-poppins mx-auto mt-4 p-4">
+      <Slider {...settings}>
+        {projects.map((project, index) => (
+          <div key={index} className="p-4">
+            <div className="shadow-lg max-sm:block flex items-center justify-center">
+              <div className='w-7/12 max-sm:w-full h-full'>
+                <img
+                  className="w-full h-full object-cover"
+                  src={logo}
+                  alt={project.title}
+                />
+              </div>
+              <div className="w-5/12 max-sm:w-full p-5 ml-5">
+                <h5 className="font-poppins font-bold mb-5">
+                  {project.title}
+                </h5>
+                <p className="font-poppins text-gray-600 mb-5 max-sm:text-sm">
+                  {project.description}
+                </p>
+                <a href={project.link} className='text-cyan-700 text-sm mt-5 mb-5 max-sm:text-xs'>View Source Code</a>
+              </div>
+            </div>
+          </div>
+        ))}
+      </Slider>
+    </Box>
+    </div>
+  )
+}
