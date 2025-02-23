8bdaaecd4f6d74e6949c755dadac304ea30874b2
hashan789 <hashanmalind123@gmail.com>
2025-02-23 12:30:24 +0530

    modified nav bar for mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,28 @@
 import { Link } from "react-scroll";
+import { FaMinusCircle } from "react-icons/fa";
+import { useState } from "react";
 
-const Navbar = () => (
+function Navbar(){
+
+  const [ isShowMenu, setIsShowMenu ] = useState(false);
+
+  const toggleMenuBar = () => {
+
+    setIsShowMenu(!isShowMenu);
+  }
+
+  return(
   <nav className="fixed top-0 w-full bg-white shadow-md z-10">
     <div className="max-w-6xl mx-auto flex justify-between items-center py-4 px-6">
       <h1 className="text-xl font-bold text-blue-600">Hash78</h1>
-      <div className="space-x-6 max-sm:w-0">
+      <div className="lg:hidden sm:hidden max-sm:block" onClick={toggleMenuBar}><FaMinusCircle size={40} className="text-black" /></div>
+      <div className={`lg:space-x-6 max-sm:p-4 max-sm:w-full max-sm:h-auto max-sm:bg-gray-600 max-sm:text-sm ${ isShowMenu ? 'max-sm:grid' : 'max-sm:hidden' } transition-all duration-300 max-sm:absolute max-sm:top-20 max-sm:left-3/4 max-sm:text-white`}>
         {["Home", "About", "experience", "Projects", "Contact"].map((section) => (
           <Link
             key={section}
             to={section.toLowerCase()}
             smooth
-            className="cursor-pointer text-gray-600 hover:text-blue-600"
+            className="cursor-pointer lg:text-gray-600 hover:text-blue-600"
           >
             {section}
           </Link>
@@ -18,6 +30,7 @@ const Navbar = () => (
       </div>
     </div>
   </nav>
-);
+  )
+}
 
 export default Navbar;
