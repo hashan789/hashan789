191d8d21fa9eddb9f7b3733ddcc300e7861ce4c1
hashan789 <hashanmalind123@gmail.com>
2025-03-28 21:47:59 +0530

    added the profile image

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,9 @@
+import portfolio_image from '../images/hashan_malinda_photo.jpeg';
+
 const About = () => (
     <section id="about" className="py-20 bg-gray-100">
       <div className="sm:block lg:flex justify-evenly items-center max-sm:text-center">
-        <div className="w-32 h-32 rounded-full bg-slate-400 border max-sm:w-0 max-sm:h-0"></div>
+        <div className="w-48 h-48 rounded-full border max-sm:w-0 max-sm:h-0" style={{ backgroundImage : `url(${portfolio_image})` , backgroundPosition : 'center' , backgroundSize : 'cover', backgroundRepeat : 'no-repeat'}}></div>
         <div className="sm:block align-left">
         <h2 className="text-3xl font-bold mb-4 text-center">About Me</h2>
         <div className="w-0 h-0 rounded-full bg-slate-400 border max-sm:w-32 max-sm:h-32 max-sm:relative max-sm:left-1/3 max-sm:mb-5"></div>
