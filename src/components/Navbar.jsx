78332bf028a6fa2b14b182547606a611708dfa66
hashan789 <hashanmalind123@gmail.com>
2025-04-01 20:26:45 +0530

    modified UI

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,38 @@
 import { Link } from "react-scroll";
 import { FaMinusCircle } from "react-icons/fa";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function Navbar(){
 
   const [ isShowMenu, setIsShowMenu ] = useState(false);
+  const [navColor, setNavColor] = useState({
+    text : "text-white",
+    bg : " ",
+    shadow : " "
+  });
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > window.innerHeight) {
+        setNavColor({
+          text : "text-gray-600",
+          bg : "bg-white",
+          shadow : "shadow-md"
+        }); // Change to any color
+      } else {
+        setNavColor({
+          text : "text-white",
+          bg : " ",
+          shadow : " "
+        }); // Default color
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [])
 
   const toggleMenuBar = () => {
 
@@ -12,7 +40,7 @@ function Navbar(){
   }
 
   return(
-  <nav className="fixed top-0 w-full bg-white shadow-md z-10">
+  <nav className={`fixed top-0 w-full ${navColor.shadow} z-10 transition-all duration-300 ${navColor.bg}`}>
     <div className="max-w-6xl mx-auto flex justify-between items-center py-4 px-6">
       <h1 className="text-xl font-bold text-blue-600">Hash78</h1>
       <div className="lg:hidden sm:hidden max-sm:block" onClick={toggleMenuBar}><FaMinusCircle size={40} className="text-black" /></div>
@@ -22,7 +50,7 @@ function Navbar(){
             key={section}
             to={section.toLowerCase()}
             smooth
-            className="cursor-pointer lg:text-gray-600 hover:text-blue-600"
+            className={`cursor-pointer lg:${navColor.text} hover:text-blue-600`}
           >
             {section}
           </Link>
