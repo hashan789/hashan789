58eabee51e83983ac088a8f775c8129b4516ac7e
hashan789 <hashanmalind123@gmail.com>
2025-04-01 21:29:23 +0530

    modified navbar for mobile design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,31 @@
 import { Link } from "react-scroll";
-import { FaMinusCircle } from "react-icons/fa";
+import { FaMinusCircle, FaHome, FaUser, FaBox, FaProjectDiagram, FaMailchimp  } from "react-icons/fa";
 import { useState, useEffect } from "react";
 
 function Navbar(){
+
+  const sections = [
+    {
+      name: "Home",
+      icon: <FaHome size={20} className="text-white lg:hidden"/>
+    },
+    {
+      name: "About",
+      icon: <FaUser size={20} className="text-white lg:hidden"/>
+    },
+    {
+      name: "Experience",
+      icon: <FaBox size={20} className="text-white lg:hidden"/>
+    },
+    {
+      name: "Projects",
+      icon: <FaProjectDiagram size={20} className="text-white lg:hidden"/>
+    },
+    {
+      name: "Contact",
+      icon: <FaMailchimp size={20} className="text-white lg:hidden"/>
+    }
+  ]
 
   const [ isShowMenu, setIsShowMenu ] = useState(false);
   const [navColor, setNavColor] = useState({
@@ -44,16 +67,18 @@ function Navbar(){
     <div className="max-w-6xl mx-auto flex justify-between items-center py-4 px-6">
       <h1 className="text-xl font-bold text-blue-600">Hash78</h1>
       <div className="lg:hidden sm:hidden max-sm:block" onClick={toggleMenuBar}><FaMinusCircle size={40} className="text-black" /></div>
-      <div className={`lg:space-x-6 max-sm:p-4 max-sm:w-full max-sm:h-auto max-sm:bg-gray-600 max-sm:text-sm ${ isShowMenu ? 'max-sm:grid' : 'max-sm:hidden' } transition-all duration-300 max-sm:absolute max-sm:top-20 max-sm:left-3/4 max-sm:text-white`}>
-        {["Home", "About", "experience", "Projects", "Contact"].map((section) => (
-          <Link
-            key={section}
-            to={section.toLowerCase()}
-            smooth
-            className={`cursor-pointer lg:${navColor.text} hover:text-blue-600`}
-          >
-            {section}
-          </Link>
+      <div className={`lg:space-x-6 lg:flex lg:justify-center lg:items-center max-sm:left-0 max-sm:p-4 max-sm:w-full max-sm:h-auto max-sm:bg-gray-600 max-sm:text-sm ${ isShowMenu ? 'max-sm:grid' : 'max-sm:hidden' } transition-all duration-300 max-sm:absolute max-sm:top-20 max-sm:text-white`}>
+        {sections.map((section, index) => (
+          <div key={index} className="max-sm:flex max-sm:justify-center max-sm:items-center max-sm:gap-3 max-sm:mb-3">
+            {section.icon}
+            <Link
+              to={section.name.toLowerCase()}
+              smooth
+              className={`cursor-pointer ${navColor.text} hover:text-blue-600`}
+            >
+              {section.name}
+            </Link>
+          </div>
         ))}
       </div>
     </div>
