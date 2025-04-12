8705e8ce913e51726ce0d64c36e9db57a233e212
hashan789 <hashanmalind123@gmail.com>
2025-04-12 16:46:45 +0530

    modified UI

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,26 @@
 const Loader = () => {
     return (
-      <div className="flex justify-center items-center h-64">
-        <div className="w-12 h-12 border-4 border-t-transparent border-white rounded-full animate-spin"></div>
+    <div className="flex items-center justify-center h-screen bg-black">
+      <svg
+        className="animate-spin h-10 w-10 text-blue-500"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+      >
+        <circle
+          className="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          fill="none"
+          strokeWidth="4"
+          stroke="currentColor"
+        />
+        <path
+          className="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 1 1 16 0A8 8 0 0 1 4 12zm2.5-1h11a2.5 2.5 0 1 1-5 0H6.5z"
+        />
+      </svg>
       </div>
     );
   };
