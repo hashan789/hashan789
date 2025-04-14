6968a49628ff6c33b778874826830863da477b69
hashan789 <hashanmalind123@gmail.com>
2025-04-14 16:49:47 +0530

    changed the loader

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import { Suspense, lazy } from 'react';
 import './App.css';
-import Loader from './components/Loader';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import BounceLoader from 'react-spinners/BounceLoader';
 
 const Homepage = lazy(() => import('./components/Homepage'));
 
 const App = () => (
   <BrowserRouter>
-    <Suspense fallback={<Loader />}>
+    <Suspense fallback={
+      <div className='w-full h-screen flex justify-center items-center bg-gray-700'>
+        <BounceLoader
+        color="#2430ca"
+        cssOverride={{}}
+        size={150}
+      />
+      </div>
+    }>
       <Routes>
         <Route path="/" element={<Homepage />} />
       </Routes>
