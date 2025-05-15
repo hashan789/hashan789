c301f06eda05b57f22a69b8717ad25a57f2ca249
hashan789 <hashanmalind123@gmail.com>
2025-05-15 21:42:52 +0530

    changed the title of file

--- src/context/downloadCV.js
+++ src/context/downloadCV.js
@@ -2,7 +2,7 @@ const downloadCV = () => {
     const pdfUrl = "/hashan_malinda.pdf";
     const link = document.createElement("a");
     link.setAttribute("href", encodeURIComponent(pdfUrl));
-    link.setAttribute('download',"document.pdf"); // specify the filename
+    link.setAttribute('download',"hashan_malinda.pdf"); // specify the filename
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
