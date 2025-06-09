77c079652727f0c6b2eb71017fe38a4b5bf2352f
hashan789 <hashanmalind123@gmail.com>
2025-06-09 13:37:37 +0530

    removed bg image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,11 +2,10 @@ import { FaLaptopCode } from "react-icons/fa";
 import { FaGithub, FaLinkedin, FaMedium } from "react-icons/fa";
 import { Button } from "react-scroll";
 import  downloadCV  from "../context/downloadCV";
-import homepage from '../images/footer_image.png';
 import TypingText from "./TypingText";
 
 const Hero = () => (
-  <section id="home" className="h-screen max-sm:bg-center max-sm:bg-cover flex flex-col justify-center items-center text-center text-white" style={{ backgroundImage : `url(${homepage})` }}>
+  <section id="home" className="h-screen max-sm:bg-center max-sm:bg-cover flex flex-col justify-center items-center text-center text-black">
     <h1 className="text-4xl md:text-6xl font-bold mb-4 animation-fade-in">
       Hi, I'm Hashan!
     </h1>
@@ -15,17 +14,17 @@ const Hero = () => (
     </div>
     <FaLaptopCode className="text-5xl animate-bounce" />
     <div className="flex justify-center items-center space-x-4 mt-4">
-      <a href="https://github.com/hashan789" className="text-white hover:text-black text-2xl">
+      <a href="https://github.com/hashan789" className="text-black hover:text-black text-2xl">
         <FaGithub />
       </a>
-      <a href="https://www.linkedin.com/in/hashan-malinda-b89474213/" className="text-white hover:text-blue-800 text-2xl">
+      <a href="https://www.linkedin.com/in/hashan-malinda-b89474213/" className="text-black hover:text-blue-800 text-2xl">
         <FaLinkedin />
       </a>
-      <a href="https://medium.com/@hashanmalind123" className="text-white hover:text-black text-2xl">
+      <a href="https://medium.com/@hashanmalind123" className="text-black hover:text-black text-2xl">
         <FaMedium />
       </a>
     </div>
-    <Button smooth className={`text-workspace-light w-36 font-semibold h-10 rounded-md bg-white mt-5`} onClick={downloadCV}>Download CV</Button>
+    <Button smooth className={`bg-workspace-light w-36 font-semibold h-10 rounded-md text-white mt-5`} onClick={downloadCV}>Download CV</Button>
   </section>
 );
 
