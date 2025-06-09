d5104c227222dc4750b36002f5280f9d013ebfd5
hashan789 <hashanmalind123@gmail.com>
2025-06-09 13:37:15 +0530

    removed vertical line

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -7,12 +7,12 @@ function WorkExperienceTree () {
   const [ref, setInView] = useInView({ threshold : 0.5 })
 
   return(
-      <section id="experience" className={`py-20 bg-workspace-dark text-black flex items-center justify-center`}>
+      <section id="experience" className={`py-20 bg-workspace-light text-black flex items-center justify-center`}>
         <div ref={ref} className={`container max-sm:block lg:flex items-center justify-evenly gap-4 transition-all duration-1000 ${ setInView ? 'opacity-100' : 'opacity-0'}`}>
           <div>
           <h2 className="text-3xl font-bold text-center max-sm:text-2xl max-sm:mb-8">Work Experience</h2>
           </div>
-          <div className="max-w-xl lg:border-l-4 lg:border-blue-500 pl-8 space-y-10">
+          <div className="max-w-xl pl-8 space-y-10">
             {experiences.map((exp, index) => (
               <div key={index} className="relative">
                 {/* Timeline Dot */}
@@ -31,7 +31,7 @@ function WorkExperienceTree () {
                       <FaLinkedin size={20} className='hover:text-blue-500 transition-all duration-300'/>
                     </a>
                   </div>
-                  <p className="mt-2 text-sm">{exp.description}</p>
+                  <p className="mt-2 lg:text-md max-md:text-sm">{exp.description}</p>
                   <span className="text-xs text-gray-700">{exp.duration}</span>
                   <div className="mt-3">
                   <a href={exp.article} className={`bg-white text-workspace-light text-sm font-semibold px-6 py-2 max-sm:mb-3 rounded-full shadow hover:bg-gray-100 transition duration-300`}>
