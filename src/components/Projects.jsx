16ceea4667306cc4ea1d348c0ff940cd039be6f8
hashan789 <hashanmalind123@gmail.com>
2025-06-09 13:39:36 +0530

    added pointer for sections' titles

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,8 +16,8 @@ function Projects(){
       <div ref={ref} className={`transition-all duration-1000 ${ setInView ? 'opacity-100' : 'opacity-0'}`}> 
         <div className="text-3xl text-center font-bold mb-6">Projects</div>
         <div className="flex justify-around items-center">
-          <h2 className={`px-5 py-2 ${section === "Web" ? 'text-workspace-dark' :  'text-black' } font-semibold lg:text-xl max-sm:text-sm`} onClick={() => setSection("Web")}>Web Development</h2>
-          <h2 className={`px-5 py-2 ${section === "ML" ? 'text-workspace-dark' :  'text-black' } font-semibold lg:text-xl max-sm:text-sm`} onClick={() => setSection("ML")}>Machine Learning</h2>
+          <h2 className={`px-5 py-2 cursor-pointer ${section === "Web" ? 'text-workspace-dark' :  'text-black' } font-semibold lg:text-xl max-sm:text-sm`} onClick={() => setSection("Web")}>Web Development</h2>
+          <h2 className={`px-5 py-2 cursor-pointer ${section === "ML" ? 'text-workspace-dark' :  'text-black' } font-semibold lg:text-xl max-sm:text-sm`} onClick={() => setSection("ML")}>Machine Learning</h2>
         </div>
           <div className="mt-5">
         {
