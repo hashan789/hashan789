61788421b9921331a250506d695d598724410e6e
hashan789 <hashanmalind123@gmail.com>
2025-06-09 13:36:44 +0530

    changed the position of form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -30,10 +30,10 @@ function Contact () {
   }
 
   return(
-  <section id="contact" className="flex items-center justify-center py-20 text-white">
+  <section id="contact" className="flex items-center justify-center pt-10 pb-20 text-white">
       <div ref={ref} className={`container max-sm:block flex items-center justify-around lg:rounded-lg bg-gray-500 transition-all duration-1000 ${ setInView ? 'opacity-100' : 'opacity-0'}`} style={{ backgroundImage : `url(${logo})` , backgroundPositionY : 'center' , }}>
       <div className="w-6/12 max-sm:hidden bg-no-repeat bg-cover bg-center bg-opacity-50 space-x-5">
-        <p className="text-6xl font-bold mb-5 text-center">Gives Best</p>
+        <p className="text-6xl font-bold mb-5 text-center">Give Best</p>
         <p className="text-sm mb-5">Have a question, project idea, or collaboration opportunity? Feel free to reach out! Whether you want to discuss Microsoft Azure solutions, AI-driven applications, React.js development, or machine learning projects, I’d love to hear from you. Fill out the form below, and I’ll get back to you as soon as possible.</p>
         <p className="text-sm">Let{"’"}s build something amazing together! 🚀</p>
       </div>
